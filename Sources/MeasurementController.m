9c33ab09b146f1db3e8aacef446954096b0a8216
Thomas Reyskens <thomas.reyskens@fibricheck.com>
2024-11-27 13:18:15 +0100

    FCS-68 Acc fix & Beta release

--- Sources/MeasurementController.m
+++ Sources/MeasurementController.m
@@ -392,17 +392,17 @@
 }
 
 - (void) detectMovementWithAccX: (float) accx accY:(float)accy accZ:(float)accz {
-    if (self.movementDetectionEnabled) {
-        double accVector =  sqrt( pow(accx,2) + pow(accy,2) + pow(accz,2) );
-        if (accVector == 0) {
-            [self notifyDelegateDidReceiveBrokenAccSensorData];
-            [self stopCamera];
-        }
-        
-        if (accVector > self.movementVectorUpperLimit || accVector < self.movementVectorLowerLimit) {
+    double accVector =  sqrt( pow(accx,2) + pow(accy,2) + pow(accz,2) );
+    if (accVector == 0 && self.movementDetectionEnabled) {
+        [self notifyDelegateDidReceiveBrokenAccSensorData];
+        [self stopCamera];
+    }
+
+    if (accVector > self.movementVectorUpperLimit || accVector < self.movementVectorLowerLimit) {
+        if (self.movementDetectionEnabled) {
             self.state = MeasurementControllerStateDetectingFinger;
-            [self notifyDelegateDidReceiveMovement];
         }
+        [self notifyDelegateDidReceiveMovement];
     }
 }
 
