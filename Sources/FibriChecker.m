a334f9e768425017cefbf633b1321205b265ea6c
Thomas Reyskens <thomas.reyskens@fibricheck.com>
2026-05-08 14:37:09 +0200

    fix: default timeouts update

--- Sources/FibriChecker.m
+++ Sources/FibriChecker.m
@@ -44,7 +44,7 @@
         self.quadrantRows = 4;
         self.quadrantCols = 4;
         self.sampleTime = 60;
-        self.pulseDetectionExpiryTime = 10000;
+        self.pulseDetectionExpiryTime = 10;
         self.fingerDetectionExpiryTime = -1;
         self.upperMovementLimit = 14;
         self.lowerMovementLimit = 6;
